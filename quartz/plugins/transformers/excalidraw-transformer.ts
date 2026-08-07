832e4d2650a92411d97374eb983d96b9f1cba007
igor <igorecha.9999@gmail.com>
2026-08-08 01:58:29 +0700

    Quartz sync: Aug 8, 2026, 1:58 AM

--- quartz/plugins/transformers/excalidraw-transformer.ts
+++ quartz/plugins/transformers/excalidraw-transformer.ts
@@ -3,13 +3,13 @@ import type { QuartzTransformerPlugin, BuildCtx } from "@quartz-community/types"
 function replaceExcalidrawLinks(text: string): string {
   const regex = /!\[\[([\s\S]*?)\.excalidraw\]\]/g;
 
-  return text.replace(regex, (match, innerContent) => {
+  return text.replace(regex, (_match, innerContent) => {
     const newLink = `<div style="overflow: auto;"><img style="max-width: 100%; height: auto;" src="Графы/${innerContent}.svg"></div>`;
     return newLink;
   });
 }
 
-export const ExcalidrawTransformer: QuartzTransformerPlugin<{ enabled: boolean }> = (options) => {
+export const ExcalidrawTransformer: QuartzTransformerPlugin<{ enabled: boolean }> = (_options) => {
   return {
     name: "ExcalidrawTransformer",
     textTransform(_ctx: BuildCtx, src: string) {
