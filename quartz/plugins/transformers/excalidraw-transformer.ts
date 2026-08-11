943801d9e8e9f2e9485baa98843d2397e6c45d6d
igor <igorecha.9999@gmail.com>
2026-08-11 18:33:16 +0700

    Quartz sync: Aug 11, 2026, 6:33 PM

--- quartz/plugins/transformers/excalidraw-transformer.ts
+++ quartz/plugins/transformers/excalidraw-transformer.ts
@@ -4,8 +4,9 @@ function replaceExcalidrawLinks(text: string): string {
   const regex = /!\[\[([\s\S]*?)\.excalidraw\]\]/g;
 
   return text.replace(regex, (_match, innerContent) => {
-    const imageSrc = `public-vault/graphs/${innerContent}.svg`
-    const openFullScreenButton = `<a href="javascript:void(window.open(new URL('${imageSrc}', window.location.origin).href, '_blank'))" 
+    const imageSrc = `Graphs/${innerContent}.svg`
+    const imageUrl = `https://igorrreha.github.io/public-vault/${imageSrc.toLowerCase()}`
+    const openFullScreenButton = `<a href="${imageUrl}" 
       style="position: absolute; top: 0px; right: 0px; width: 32px; height: 32px; display: flex; align-items: center; justify-content: center; background: none; color: #ffffff55; text-decoration: none; font-family: sans-serif; font-size: 14px; border-radius: 6px; box-sizing: border-box;">
       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
         <path d="M15 3h6v6M9 21H3v-6M21 3l-7 7M3 21l7-7"/>
