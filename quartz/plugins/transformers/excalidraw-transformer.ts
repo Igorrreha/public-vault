9fa33c923db332431d22aa2b29e42bdd5dd13190
igor <igorecha.9999@gmail.com>
2026-08-11 18:21:15 +0700

    Quartz sync: Aug 11, 2026, 6:21 PM

--- quartz/plugins/transformers/excalidraw-transformer.ts
+++ quartz/plugins/transformers/excalidraw-transformer.ts
@@ -4,7 +4,7 @@ function replaceExcalidrawLinks(text: string): string {
   const regex = /!\[\[([\s\S]*?)\.excalidraw\]\]/g;
 
   return text.replace(regex, (_match, innerContent) => {
-    const imageSrc = `Graphs/${innerContent}.svg`
+    const imageSrc = `public-vault/graphs/${innerContent}.svg`
     const openFullScreenButton = `<a href="javascript:void(window.open(new URL('${imageSrc}', window.location.origin).href, '_blank'))" 
       style="position: absolute; top: 0px; right: 0px; width: 32px; height: 32px; display: flex; align-items: center; justify-content: center; background: none; color: #ffffff55; text-decoration: none; font-family: sans-serif; font-size: 14px; border-radius: 6px; box-sizing: border-box;">
       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
